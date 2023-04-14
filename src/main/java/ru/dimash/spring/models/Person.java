295d5f462b161bfa692f803f286f7bb949899d0e
Dimash <d.esengeldiev@mail.ru>
2023-04-14 21:19:25 +0600

    add models add controllers and views

---
+++ src/main/java/ru/dimash/spring/models/Person.java
@@ -0,0 +1,36 @@
+package ru.dimash.spring.models;
+
+public class Person {
+    private int id;
+    private String fullname;
+    private int birthday;
+
+    public Person(String fullname, int birthday) {
+        this.fullname = fullname;
+        this.birthday = birthday;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+}
