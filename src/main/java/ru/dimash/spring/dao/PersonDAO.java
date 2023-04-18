8bfbbda37aa8bacbccb04019d4c2da0a522a779c
Dimash <d.esengeldiev@mail.ru>
2023-04-19 00:44:36 +0600

    jdbc update create delete show

--- src/main/java/ru/dimash/spring/dao/PersonDAO.java
+++ src/main/java/ru/dimash/spring/dao/PersonDAO.java
@@ -55,30 +55,55 @@ public class PersonDAO {
     }
 
     public Person show(int id){
-        return null;
+        Person person = null;
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Person WHERE id=?");
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setFullname(resultSet.getString("fullname"));
+            person.setBirthday(resultSet.getInt("birthday"));
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return person;
     }
 
     public void save(Person person){
         try {
-            Statement statement = connection.createStatement();
-            String SQL = "INSERT INTO Person VALUES(" + person.getId() + ",'" + person.getFullname() +
-                    "'," + person.getBirthday() + ")";
-            statement.executeUpdate(SQL);
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Person VALUES (1,?,?)");
+            preparedStatement.setString(1, person.getFullname());
+            preparedStatement.setInt(2, person.getBirthday());
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void update(int id, Person updatePerson){
-
-//        Person personToBeUpdated = show(id);
-//        personToBeUpdated.setFullname(updatePerson.getFullname());
-//        personToBeUpdated.setBirthday(updatePerson.getBirthday());
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE Person SET fullname=?, birthday=? WHERE id=? ");
+            preparedStatement.setString(1, updatePerson.getFullname());
+            preparedStatement.setInt(2, updatePerson.getBirthday());
+            preparedStatement.setInt(3, updatePerson.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void delete(int id){
 
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id=?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
